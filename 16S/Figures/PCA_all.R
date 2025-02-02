5d6312fe681b3a4003ca055ff738375cbd457496
Amelie Baud <abaud@node-hp0414.linux.crg.es>
2025-02-02 19:00:04 +0100

    PCA_all.R updated to work on each taxonomic level

--- 16S/Figures/PCA_all.R
+++ 16S/Figures/PCA_all.R
@@ -1,23 +1,30 @@
 load('/users/abaud/data/secondary/P50_HSrats/felipes_deblur/collapsed_full_biomt_collapsed_clr_counts.RData')
 # collapsed_clr_counts is post CLR post collapsing to higher level taxa
-collapsed_clr_counts = collapsed_clr_counts[grep('g__', rownames(collapsed_clr_counts)),]
-pca = prcomp(t(collapsed_clr_counts))
-vars = round(pca$sdev^2 / (sum(pca$sdev^2))*100, digits = 1)
+save_collapsed_clr_counts = collapsed_clr_counts
 
-load('/nfs/users/abaud/abaud/P50_HSrats/data/metadata/metadata_augmented_16S_metabo_deblur.RData')
-motch = match(colnames(collapsed_clr_counts), metadata$deblur_rooname)
-any(is.na(motch))
-#FALSE
-metadata = metadata[motch,]
+pdf('/nfs/users/abaud/abaud/P50_HSrats/plots/PCA_paper.pdf')
 
-cols = rep('white', dim(collapsed_clr_counts)[2])
-cols[metadata$study == 'NY'] = 'blue'
-cols[metadata$study == 'MI'] = 'green'
-cols[metadata$study == 'TN_behavior'] = 'red'
-cols[metadata$study == 'TN_breeder'] = 'orange'
+for (tax_level in c("p__","c__","o__","f__","g__")) {
 
-pdf('/nfs/users/abaud/abaud/P50_HSrats/plots/PCA_all_genera.pdf')
-plot(pca$x[,1], pca$x[,2], xlab = 'MDS1 (24%)', ylab = 'MDS2 (8%)', col = cols, pch = 16, las = 1, cex = 0.7)
-legend('topright', fill = c('blue','green','red','orange'), legend = c('NY','MI','TN1','TN2'), border = NA, box.col= NA)
+	collapsed_clr_counts = save_collapsed_clr_counts[grep(tax_level, rownames(save_collapsed_clr_counts)),]
+	pca = prcomp(t(collapsed_clr_counts))
+	vars = round(pca$sdev^2 / (sum(pca$sdev^2))*100, digits = 0)
+
+	load('/nfs/users/abaud/abaud/P50_HSrats/data/metadata/metadata_augmented_16S_metabo_deblur.RData')
+	motch = match(colnames(collapsed_clr_counts), metadata$deblur_rooname)
+	any(is.na(motch))
+	#FALSE
+	metadata = metadata[motch,]
+
+	cols = rep('white', dim(collapsed_clr_counts)[2])
+	cols[metadata$study == 'NY'] = 'blue'
+	cols[metadata$study == 'MI'] = 'green'
+	cols[metadata$study == 'TN_behavior'] = 'red'
+	cols[metadata$study == 'TN_breeder'] = 'orange'
+
+	plot(pca$x[,1], pca$x[,2], xlab = paste('MDS1 (',vars[1],'%)', sep=''), ylab = paste('MDS2 (',vars[2],'%)', sep=''), col = cols, pch = 16, las = 1, cex = 0.7, main = tax_level)
+	legend('topright', fill = c('blue','green','red','orange'), legend = c('NY','MI','TN1','TN2'), border = NA, box.col= NA)
+
+}
 dev.off()
 
