fd97b3bf0981bb2ba6df377ee449376b022d93cd
tonnelene <helene.tonnele@crg.eu>
2025-03-14 10:15:03 +0100

    2025.03.14: small changes

--- 16S/Figures/total_genetic_variance_barplot.R
+++ 16S/Figures/total_genetic_variance_barplot.R
@@ -22,7 +22,7 @@ asv_VCs = VCs
 sel = all_VCs_full[order(all_VCs_full$pvalue_DGE, decreasing = F),][1:3,]
 selDGE= asv_VCs[asv_VCs$trait1 %in% sel$trait1,]
   
-# total herit is 2.6-3.2 times greater than classical heritability across these 3 phenotypes
+# total herit is ... times greater than classical heritability across these 3 phenotypes
 sel$total_heritability / selDGE$prop_Ad1 # 
 
 #sel$tot_her_IGE = sel$prop_Ad1 + 2*(2-1)*sel$corr_Ad1s1*sqrt(sel$prop_Ad1*sel$prop_As1) + (2-1)^2*sel$prop_As1
@@ -108,3 +108,120 @@ legend("topright", fill = coolors,
 
 dev.off()
 
+
+
+###########
+# checking if all DGE alone < DGE when IGE
+rownames(all_VCs_full) = all_VCs_full$trait1
+rownames(asv_VCs) = asv_VCs$trait1
+#sum(rownames(asv_VCs) %in% rownames(all_VCs_full))
+#sum(rownames(all_VCs_full) %in% rownames(asv_VCs))
+
+all_VCs_full = all_VCs_full[rownames(all_VCs_full) %in% rownames(asv_VCs),] # 1st element in asv_VCs corresponds to 283 el in all_VCs_full
+asv_VCs = asv_VCs[rownames(all_VCs_full),]
+all(rownames(all_VCs_full) == rownames(asv_VCs))
+
+sum(asv_VCs$prop_Ad1 < all_VCs_full$prop_Ad1)
+
+plotdots <- function(x,y,xylim=T,line="xy",...){
+  pch=16
+  if(xylim){ axlim = range(c(x,y))}else{axlim = NULL}
+  #print(axlim)}
+  plot(x, y,
+       xlim = axlim, ylim = axlim,type="n",
+       ...)
+  if(line=="xy") abline(b=1,a=0, col="grey", lty=2)
+  if(line=="cor") abline(lm(y~x), col="grey", lty=2)
+  points(x,y, col=dot.col,pch=pch)
+  
+  legend("topleft", pch=pch, col=coolors, 
+         legend = lgd)
+  if(line=="cor") mtext(paste0("cor(x,y) = ",round(cor(x,y), 2)), line=-1)
+}
+
+coolors=c("red","orange")
+thrsh = c(0.01,0.05)
+
+dot.col = rep("grey40", nrow(all_VCs_full))
+dot.col[all_VCs_full$prop_As1 > thrsh[1]] = coolors[1]
+dot.col[all_VCs_full$prop_As1 > thrsh[1] & all_VCs_full$pvalue_DGE < thrsh[2]] = coolors[2]
+
+lgd = c(paste0("IGE > ",thrsh[1]), paste0("& p-val < ", thrsh[2]))
+
+pdf("/users/abaud/htonnele/PRJs/P50_HSrats/16S/plot/tot_herit_vs_DGE.pdf")
+plotdots(all_VCs_full$prop_Ad1, asv_VCs$prop_Ad1, 
+         xlab="DGE(with IGE)", ylab = "DGE(alone)")
+plotdots(all_VCs_full$total_heritability, asv_VCs$prop_Ad1, 
+         xlab="tot_herit(with IGE)", ylab = "DGE(alone)")
+dev.off()
+
+
+pdf("/users/abaud/htonnele/PRJs/P50_HSrats/16S/plot/tot_herit_vs_DGE_corcol.pdf")
+coolors=c("red","orange","#4DBBD5FF")
+thrsh = c(0.01,0.05,0.9)
+dot.col[all_VCs_full$prop_As1 > thrsh[1] & all_VCs_full$pvalue_DGE < thrsh[2] & all_VCs_full$corr_Ad1s1 > thrsh[3]] = coolors[3]
+lgd = c(paste0("IGE > ",thrsh[1]), paste0("& p-val < ", thrsh[2]) , paste0("& cor(DGE,IGE) > ", thrsh[3]) )
+
+plotdots(all_VCs_full$prop_Ad1, asv_VCs$prop_Ad1, 
+         xlab="DGE(with IGE)", ylab = "DGE(alone)")
+plotdots(all_VCs_full$total_heritability, asv_VCs$prop_Ad1, 
+         xlab="tot_herit(with IGE)", ylab = "DGE(alone)")
+dev.off()
+
+##-> could you check whether the largest differences are for the most positive corr_Ad1s1?
+## plot diff (asv_VCs$prop_Ad1 - all_VCs_full$prop_Ad1) or (asv_VCs$prop_Ad1 - all_VCs_full$total_heritability) VS all_VCs_full$corr_Ad1s1
+pdf("/users/abaud/htonnele/PRJs/P50_HSrats/16S/plot/tot_herit_delta_DGE_vs_corr_corcol.pdf")
+
+plotdots(all_VCs_full$corr_Ad1s1, (all_VCs_full$prop_Ad1 - asv_VCs$prop_Ad1 ),
+         xlab = "cor(IGE,DGE)", ylab="DGE(with IGE) - DGE(alone)", 
+         xylim=F, line="cor")
+#cor.test(abs(all_VCs_full$corr_Ad1s1), (all_VCs_full$prop_Ad1 - asv_VCs$prop_Ad1 ))
+#lm((all_VCs_full$prop_Ad1 - asv_VCs$prop_Ad1) ~ all_VCs_full$corr_Ad1s1)
+
+plotdots(all_VCs_full$corr_Ad1s1, (all_VCs_full$total_heritability - asv_VCs$prop_Ad1 ),
+         xlab = "cor(IGE,DGE)", ylab="tot_herit(with IGE) - DGE(alone)", 
+         xylim=F, line="cor")
+dev.off()
+
+
+######### 
+boxdots = function(y,...){
+  set.seed(12)
+  pch=16
+  boxplot(y, 
+          outline = F, 
+          ylim=range(y),
+          boxwex=0.8,
+          col=alpha("white",0), 
+          border=alpha("white",0),...)
+          #xlab = "DGE(with IGE) - DGE(alone)")
+  points(jitter(rep(1, nrow(all_VCs_full)), factor=7), 
+         y, 
+         ylab='', col = dot.col,
+         pch=pch, cex=1.2)
+  boxplot(y, 
+          outline = F, 
+          boxwex=0.8,
+          col=alpha("white",0.5), 
+          #border=alpha("white",0),
+          yaxt="n",
+          add=T)
+}
+
+coolors=c("red","orange","#4DBBD5FF")
+thrsh = c(0.01,0.05,0.9)
+dot.col = rep("grey40", nrow(all_VCs_full))
+dot.col[all_VCs_full$prop_As1 > thrsh[1]] = coolors[1]
+dot.col[all_VCs_full$prop_As1 > thrsh[1] & all_VCs_full$pvalue_DGE < thrsh[2]] = coolors[2]
+dot.col[all_VCs_full$prop_As1 > thrsh[1] & all_VCs_full$pvalue_DGE < thrsh[2] & all_VCs_full$corr_Ad1s1 > thrsh[3]] = coolors[3]
+
+lgd = c(paste0("IGE > ",thrsh[1]), paste0("& p-val < ", thrsh[2]) , paste0("& cor(DGE,IGE) > ", thrsh[3]))
+pch=16
+
+pdf("/users/abaud/htonnele/PRJs/P50_HSrats/16S/plot/tot_herit_delta_DGE_vs_corr_bplot.pdf")
+par(mfrow=c(1,2))
+boxdots(all_VCs_full$prop_Ad1 - asv_VCs$prop_Ad1, ylab="DGE(with IGE) - DGE(alone)")
+legend("topleft", pch=pch, col=coolors, 
+       legend = lgd, xpd=T, inset=c(0,-0.12), cex = 0.8, bg="white")
+boxdots(all_VCs_full$total_heritability - asv_VCs$prop_Ad1, ylab="tot_herit(with IGE) - DGE(alone)")
+dev.off()
